1d480ed1d0519651f971e80d1bd4e7c9141bc656
tocchimittsu <arimagedon21@gmail.com>
2021-05-11 07:02:22 +0900

    修正

--- main.py
+++ main.py
@@ -6,8 +6,20 @@ df = pd.read_csv('./sample.csv')
 # お店の一覧を格納する変数を宣言する。
 shopNameList = []
 
-# 最終的に出力するデータ群を宣言する。
-outputData = [['A', 'B', 'C', 'D', 'E']]
+# 商品一覧を格納する変数を宣言する。
+productList = []
+
+# 2次元配列 → 1次元配列へ変換する。
+# 2次元配列 → 1次元配列へ変換する際に、改行処理も施す。
+
+
+def convertProductList(list):
+    tmp = []
+    for l in list:
+        # joinについて : https://www.headboost.jp/python-strings-into-a-list/#1_join
+        tmp.append("\n".join(l))
+    return tmp
+
 
 # csv情報を1行ずつ読み出す。
 # iterrows参考 : https://note.nkmk.me/python-pandas-dataframe-for-iteration/
@@ -20,22 +32,23 @@ for index, row in df.iterrows():
     # 既にお店一覧(shopNameList)内にお店が登録されているのか確認する。
     # 登録されている場合
     if shopName in shopNameList:
-        # 最終的なデータ形式として、[[A店, 商品A, 商品B], [B店, 商品A, 商品C], [C店, 商品B] ...]を目指す。
         # 既存で登録されているお店が格納される位置をindex関数を利用して探索する。
         # indexについて : https://www.javadrive.jp/python/list/index10.html
-        # +1しているのは、outputDataの一行目にA, B, C, D, Eが既に格納されているため。
         # appendを活用して、商品の名前を末尾に格納する。
-        outputData[shopNameList.index(shopName) + 1].append(productName)
+        productList[shopNameList.index(shopName)].append(productName)
     # 登録されていない場合
     else:
-        # 最終的なデータ形式として、[[A店, 商品A, 商品B], [B店, 商品A, 商品C], [C店, 商品B] ...]を目指す。
-        # 新規お店の登録と商品の登録を行う。
-        outputData.append([shopName, productName])
+        # 新規の商品登録を行う。index位置は新規のお店と揃える。
+        productList.append([productName])
         # 新規でお店を登録する。
         shopNameList.append(shopName)
 
-# 2次元配列をデータフレーム形式に変更する。
 # csvファイル名をoutput.csvとしてcsvを生成する。
-# header, indexを付与しないように、header=False, index=Falseとする。
+# indexを付与しないように、index=Falseとする。
 # to_csvについて : https://note.nkmk.me/python-pandas-to-csv/
-pd.DataFrame(outputData).to_csv('./output.csv', header=False, index=False)
+# B列にて2次元配列のままだと、DataFrameが使えないので、convertProductList関数を呼び出し、1次元配列への変換を行う。
+# 2次元配列 → 1次元配列へ変換する際に、改行処理も施す。
+pd.DataFrame({
+    'A': shopNameList,
+    'B': convertProductList(productList)
+}).to_csv('./output.csv', index=False)
